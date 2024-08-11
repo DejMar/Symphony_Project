3aed935e54d5e01db0ea739da4ce53b285f1c167
DejMar <marjanovic.dejan@gmail.com>
2024-08-11 19:34:57 +0200

    align code

--- helper/SharedSteps.js
+++ helper/SharedSteps.js
@@ -10,22 +10,22 @@ export class SharedSteps {
     async compareJsonFiles(filePath1, fileName1, filePath2, fileName2) {
         const fullFilePath1 = path.join(__dirname, filePath1, fileName1);
         const fullFilePath2 = path.join(__dirname, filePath2, fileName2);
-    
+
         const file1 = JSON.parse(fs.readFileSync(fullFilePath1, 'utf8'));
         const file2 = JSON.parse(fs.readFileSync(fullFilePath2, 'utf8'));
-    
+
         return JSON.stringify(file1) === JSON.stringify(file2);
     }
     async compareTxtFiles(filePath1, fileName1, filePath2, fileName2) {
         const fullFilePath1 = path.join(__dirname, filePath1, fileName1);
         const fullFilePath2 = path.join(__dirname, filePath2, fileName2);
-    
+
         const file1Content = fs.readFileSync(fullFilePath1, 'utf8');
         const file2Content = fs.readFileSync(fullFilePath2, 'utf8');
-    
+
         return file1Content === file2Content;
     }
-    
+
     async acceptCookies() {
         try {
             await this.page.waitForSelector(this.cookieOKButton, { state: 'visible', timeout: 5000 });
@@ -41,7 +41,7 @@ export class SharedSteps {
 
         const randomOption = await this.page.evaluate((sel) => {
             const select = document.querySelector(sel);
-            const options = Array.from(select.options); 
+            const options = Array.from(select.options);
             const randomIndex = Math.floor(Math.random() * options.length);
             const selectedOption = options[randomIndex];
             select.value = selectedOption.value;
@@ -58,9 +58,9 @@ export class SharedSteps {
 
     async takeScreenshotOnFailure(page, testInfo) {
         if (testInfo.status !== 'passed') {
-          const screenshotPath = `screenshots/${testInfo.title.replace(/\s+/g, '_')}_${new Date().toISOString().split('T')[0]}.png`;
-          await page.screenshot({ path: screenshotPath, fullPage: true });
-          console.log(`Screenshot saved: ${screenshotPath}`);
+            const screenshotPath = `screenshots/${testInfo.title.replace(/\s+/g, '_')}_${new Date().toISOString().split('T')[0]}.png`;
+            await page.screenshot({ path: screenshotPath, fullPage: true });
+            console.log(`Screenshot saved: ${screenshotPath}`);
         }
-      }
+    }
 }
